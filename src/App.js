9ef647fdde7aaad4195e371ae2952c2465ada2ab
Andrew Sleigh <andrewsleigh@akauk.com>
2019-03-10 19:19:27 +0000

    components folder added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import MovieCard from './components/MovieCards'
+
+const originalMovies = [
+  {id: 1, title: 'Star Wars'},
+  {id: 2, title: 'Cest ici'},
+  {id: 3, title: 'Cnest pas absolomant fantastique'}
+]
+
 class App extends Component {
+  state = { movies: [] }
+
+  componentDidMount(){
+    this.setState({
+      movies: originalMovies
+    })
+  }
   render() {
+    const {movies} = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      {movies.map(movie => 
+       <MovieCard key={movie.id} movie={movie} />
+       
+      )}
+       
       </div>
     );
   }
---
+++ src/components/MovieCards.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+class MovieCard extends React.Component{
+    render(){
+        const {movie} = this.props
+        return (
+            <div>
+                <h1>{movie.title}</h1>
+
+            </div>
+        )
+    }
+}
+
+export default MovieCard;
