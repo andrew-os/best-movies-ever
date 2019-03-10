6e361603c7ed6ec5ad09885945a003e4becfd40b
Andrew Sleigh <andrewsleigh@akauk.com>
2019-03-10 19:33:49 +0000

    async fetching data, on mount stored in const set state movies object to const json results map over and render in component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import MovieCard from './components/MovieCards'
+import apiKey from './apiKey';
 
+import MovieCard from './components/MovieCards'
+//https://api.themoviedb.org/3/movie/top_rated?api_key=8d25ce050ebd0627fcf07f8002011b71&language=en-US&page=1
 const originalMovies = [
   {id: 1, title: 'Star Wars'},
   {id: 2, title: 'Cest ici'},
@@ -12,10 +14,14 @@ const originalMovies = [
 class App extends Component {
   state = { movies: [] }
 
-  componentDidMount(){
-    this.setState({
-      movies: originalMovies
-    })
+  async componentDidMount(){
+      // movies: originalMovies
+      const response = await fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}&language=en-US&page=1`)
+      const json = await response.json();
+      this.setState({
+        movies : json.results
+      })
+
   }
   render() {
     const {movies} = this.state;
